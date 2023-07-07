14aef3e1e51cbc195f9cc255e7aea2404b6468be
tuttertep <tuttertep@gmail.com>
2023-07-07 09:14:16 +0300

    v0.027

--- comfy_map.lua
+++ comfy_map.lua
@@ -1,10 +1,11 @@
 --made by tuttertep
 --i'm already sorry if you read this because everything below is a disaster
 
---ui.setAsynchronousImagesLoading(true)
+ui.setAsynchronousImagesLoading(true)
 local default_colors = '0,0.5,0,0.9|0,0,0.5,0.9|0.5,0,0,0.9|1,0.5,0.5,1|0.8,0,0,1|1,1,1,1'
 local settings = ac.storage {
   centered = false,
+  rotation = true,
   teleporting = true,
   friends = true,
   names = true,
@@ -12,6 +13,7 @@ local settings = ac.storage {
   namesy = 0,
   colors = default_colors,
   arrowsize = 10,
+  --names_size = 10,
   names_mouseover = true,
   names_spectate = false
 }
@@ -27,19 +29,15 @@ local function tocolor(string)
   local temp = string:split('|')
   local temp1 = {}
   for i = 1, #temp do
-    for a, b, c, d in temp[i]:gmatch('(.+),(.+),(.+),(.+)') do temp1[i] = rgbm(tonumber(a), tonumber(b), tonumber(c),
-        tonumber(d))
-    end
+    for a, b, c, d in temp[i]:gmatch('(.+),(.+),(.+),(.+)') do temp1[i] = rgbm(tonumber(a), tonumber(b), tonumber(c), tonumber(d)) end
   end
   for i, k in ipairs(color_names) do
-    if temp1[i] == nil then temp1[i] = rgbm(1, 1, 1, 1) end
+    if temp1[i] == nil then temp1[i] = rgbm.colors.white end
     colors[k] = temp1[i]
   end
   return temp1
 end
-
 tocolor(settings.colors)
-
 
 
 local pos3, dir3, pos2, dir2, dir2x = vec3(), vec3(), vec2(), vec2(), vec2()
@@ -58,10 +56,17 @@ if ac.getPatchVersionCode() >= 2000 then
   end
   ui.decodeImage(map)
   ini = ac.getFolder(ac.FolderID.ContentTracks) .. '/' .. ac.getTrackFullID('/') .. '/data/map.ini'
-  for a, b in ac.INIConfig.load(ini):serialize():gmatch('([_%a]+)=([-%d.]+)') do asd[a] = tonumber(b) end
-  image_size = ui.imageSize(map)
+  --for a, b in ac.INIConfig.load(ini):serialize():gmatch('([_%a]+)=([-%d.]+)') do asd[a] = tonumber(b) end
+  local asdf = ac.INIConfig.load(ini)
+  asd['SCALE_FACTOR'] = asdf:get('PARAMETERS','SCALE_FACTOR',1)
+  asd['Z_OFFSET'] = asdf:get('PARAMETERS','Z_OFFSET',1)
+  asd['X_OFFSET'] = asdf:get('PARAMETERS','X_OFFSET',1)
   config_offset = vec2(asd.X_OFFSET, asd.Z_OFFSET)
+
+
+  image_size = ui.imageSize(map)
 end
+
 
 
 function script.windowMain(dt)
@@ -70,10 +75,9 @@ function script.windowMain(dt)
   if ac.getPatchVersionCode() < 2000 then ui.text(versionerror) return end
   ui.invisibleButton()
 
-
   if first then
+    image_size = ui.imageSize(map)
     map_scale = math.min((ui.windowWidth() - padding.x) / image_size.x, (ui.windowHeight() - padding.y) / image_size.y)
-    --if settings.centered then map_scale = map_scale * asd.SCALE_FACTOR * 1 end
     if settings.centered then map_scale = 0.5 end
     config_scale = map_scale / asd.SCALE_FACTOR
     size = image_size * map_scale
@@ -118,10 +122,7 @@ function script.windowMain(dt)
 
   if ui.windowHovered() then --zoom&drag
     if ac.getUI().mouseWheel ~= 0 then
-      if (
-          ac.getUI().mouseWheel < 0 and
-              (size.x + padding.x > ui.windowWidth() and size.y + padding.y > ui.windowHeight())) or
-          ac.getUI().mouseWheel > 0 then
+      if (ac.getUI().mouseWheel < 0 and (size.x + padding.x > ui.windowWidth() and size.y + padding.y > ui.windowHeight())) or ac.getUI().mouseWheel > 0 then
         local old = size
         map_scale = map_scale * (1 + ac.getUI().mouseWheel * 0.15)
         size = ui.imageSize(current_map) * map_scale
@@ -129,35 +130,34 @@ function script.windowMain(dt)
         offsets = (offsets + (size - old) * (offsets + ui.mouseLocalPos()) / old)
       else
         offsets = -padding * 0.5
-        map_scale = math.min((ui.windowWidth() - padding.x) / image_size.x,
-          (ui.windowHeight() - padding.y) / image_size.y)
+        map_scale = math.min((ui.windowWidth() - padding.x) / image_size.x, (ui.windowHeight() - padding.y) / image_size.y)
         size = ui.imageSize(current_map) * map_scale
         config_scale = map_scale / asd.SCALE_FACTOR
       end
     end
   end
 
-
   focusedCar = ac.getCar(ac.getSim().focusedCar)
   if settings.centered then --center on car and rotate
-    offsets:set(focusedCar.position.x, focusedCar.position.z):add(config_offset):scale(config_scale):add(-
-      ui.windowSize() / 2) --autocenter
+    offsets:set(focusedCar.position.x, focusedCar.position.z):add(config_offset):scale(config_scale):add(-ui.windowSize() / 2) --autocenter
 
-    rotationangle = 180 - math.deg(math.atan2(focusedCar.look.x, focusedCar.look.z))
-    rotation = mat4x4.rotation(math.rad(rotationangle), vec3(0, 1, 0))
-    ui.beginRotation()
+    if settings.rotation then
+      rotationangle = 180 - math.deg(math.atan2(focusedCar.look.x, focusedCar.look.z))
+      rotation = mat4x4.rotation(math.rad(rotationangle), vec3(0, 1, 0))
+      ui.beginRotation()
+    end
   end
 
+  ac.perfBegin('map&players')
   ui.drawImage(current_map, -offsets, -offsets + size, colors.map) --map image
-  if settings.centered then ui.endPivotRotation(rotationangle + 90, ui.windowSize() / 2) end
-
+  if settings.centered and settings.rotation then ui.endPivotRotation(rotationangle + 90, ui.windowSize() / 2) end
 
   for i = ac.getSim().carsCount - 1, 0, -1 do --draw stuff on map
     local car = ac.getCar(i)
     if car.isConnected and (not car.isHidingLabels) then
       local pos3 = car.position
       local dir3 = car.look
-      if settings.centered then
+      if settings.centered and settings.rotation then
         pos3 = rotation:transformPoint(car.position - focusedCar.position) + focusedCar.position
         dir3 = rotation:transformPoint(car.look)
       end
@@ -177,27 +177,23 @@ function script.windowMain(dt)
         color)
       if settings.names then
         if not (settings.names_mouseover and not ui.windowHovered()) then
-          ui.pushFont(ui.Font.Small)
-          ui.setCursor(pos2 + namepos - ui.measureText(ac.getDriverName(i)) * 0.5)
-          ui.drawLine(pos2, pos2 + namepos , color, 2)
-          --ui.dwriteDrawText(ac.getDriverName(i),10,pos2 + namepos - ui.measureText(ac.getDriverName(i)) * 0.5,rgbm.colors.white)
-          ui.text(ac.getDriverName(i))
-          ui.popFont()
-          if ui.itemClicked() and settings.names_spectate then ac.focusCar(i) end
+          drawName(i,pos2,color)
         end
       end
     end
   end
+  ac.perfEnd('map&players')
+
   local iconpos, iconsize = vec2(), vec2(5, 5)
-  if ui.windowHovered() then
+  if ui.windowHovered() then --teleports
   if ac.getSim().isOnlineRace and settings.teleporting then
     for i = 1, #teleports1 do
       local teleport_position = teleports1[i].POS
       local teleport_name = (teleports1[i].GROUP ~= nil and teleports1[i].GROUP .. '/' or "") .. teleports1[i].POINT
-      if settings.centered then teleport_position = (rotation:transformPoint(teleport_position - focusedCar.position) + focusedCar.position) end
+      if settings.centered and settings.rotation then teleport_position = (rotation:transformPoint(teleport_position - focusedCar.position) + focusedCar.position) end
       iconpos:set(teleport_position.x, teleport_position.z):add(config_offset):scale(config_scale):add(-offsets)
       local color = ac.canTeleportToServerPoint(i - 1) and colors.teleport_available or colors.teleport_unavailable
-      ui.drawCircleFilled(iconpos, 4, color, 20)
+      ui.drawCircleFilled(iconpos, iconsize.x-1, color, 20)
       if teleports1[i].HEADING ~= nil then
         local h = math.rad(teleports1[i].HEADING + ac.getCompassAngle(vec3(0, 0, 1)))
         local heading = vec2(math.sin(h), math.cos(h))
@@ -211,11 +207,10 @@ function script.windowMain(dt)
     end
   end
   end
-
-  if ac.getSim().cameraMode == ac.CameraMode.Free then
+  if ac.getSim().cameraMode == ac.CameraMode.Free then --freecam stuff
     pos3 = ac.getCameraPosition()
     dir3 = ac.getCameraForward()
-    if settings.centered then
+    if settings.centered and settings.rotation then
       pos3 = rotation:transformPoint(pos3 - focusedCar.position) + focusedCar.position
       dir3 = rotation:transformPoint(dir3)
     end
@@ -226,23 +221,23 @@ function script.windowMain(dt)
       pos2 - dir2 * ts - dir2x * ts * 0.75,
       pos2 - dir2 * ts + dir2x * ts * 0.75,
       colors.you)
-
-    if ui.keyPressed(ui.Key.Space) and ui.windowHovered() then
+    if ui.keyPressed(ui.Key.Space) and ui.windowHovered() then --freecam teleport
       local camerapos = (ui.mouseLocalPos() + offsets) / config_scale - config_offset
-      if ac.hasTrackSpline() then
-        local progress = ac.worldCoordinateToTrackProgress(camerapos)
-        local splinepos = ac.trackProgressToWorldCoordinate(progress)
-        camerapos:set(splinepos.x, splinepos.z)
-      end
-      local raycast = physics.raycastTrack(vec3(camerapos.x, 2000, camerapos.y), vec3(0, -1, 0), 3000)
-      local cameraheight = 2000 - raycast + 3
+      --if ac.hasTrackSpline() then
+      --  local progress = ac.worldCoordinateToTrackProgress(camerapos)
+      --  local splinepos = ac.trackProgressToWorldCoordinate(progress)
+      --  camerapos:set(splinepos.x, splinepos.z)
+      --end
+      local raycastheight = 3000
+      local raycast = physics.raycastTrack(vec3(camerapos.x, raycastheight, camerapos.y), vec3(0, -1, 0), raycastheight*2)
+      local cameraheight = raycastheight - raycast + 3
       if raycast ~= -1 then
         ac.setCurrentCamera(ac.CameraMode.Free)
         ac.setCameraPosition(vec3(camerapos.x, cameraheight, camerapos.y))
       end
     end
   end
-  if ui.mouseBusy() then
+  if ui.windowHovered() then
     if ui.mouseClicked(2) then
       if settings.centered then
         offsets = -padding * 0.5
@@ -267,6 +262,7 @@ local selectedcolor = 'you'
 local teleportindex = '0'
 function script.windowMainSettings(dt)
   ui.text('made by tuttertep')
+  if settings.centered then ui.text('middle click to stop rotation and centering') end
   --ui.text('middle click map to toggle centering')
   if ui.checkbox("teleports", settings.teleporting) then settings.teleporting = not settings.teleporting end
   if ui.checkbox("friends", settings.friends) then settings.friends = not settings.friends end
@@ -277,8 +273,10 @@ function script.windowMainSettings(dt)
     ui.beginGroup()
     if ui.checkbox("only on mouseover", settings.names_mouseover) then settings.names_mouseover = not settings.names_mouseover end
     if ui.checkbox("click to spectate", settings.names_spectate) then settings.names_spectate = not settings.names_spectate end
+    if ui.checkbox("rotate while centered", settings.rotation) then settings.rotation = not settings.rotation end
     settings.namesx = ui.slider('##' .. 'name x offset', settings.namesx, -100,100, 'name x offset' .. ': %.0f')
     settings.namesy = ui.slider('##' .. 'name y offset', settings.namesy,-100,100, 'name y offset:' .. ': %.0f')
+    --settings.names_size = ui.slider('##' .. 'font size', settings.names_size, 5, 50, 'font size' .. ': %.0f')
     namepos = vec2(settings.namesx, settings.namesy)
     ui.endGroup()
   end
@@ -314,12 +312,11 @@ function script.windowMainSettings(dt)
       '_GROUP= group\nPOINT_' ..
       teleportindex ..
       '_POS= ' ..
-      math.floor(ac.getCameraPosition().x * 10) / 10 ..
+      math.round(ac.getCameraPosition().x,1) ..
       "," ..
-      math.floor((ac.getCameraPosition().y - physics.raycastTrack(ac.getCameraPosition(), vec3(0, -1, 0), 20) + 0.5) * 10)
-      / 10 ..
+      math.round((ac.getCameraPosition().y - physics.raycastTrack(ac.getCameraPosition(), vec3(0, -1, 0), 20) + 0.5),1)..
       ',' ..
-      math.floor(ac.getCameraPosition().z * 10) / 10 ..
+      math.round(ac.getCameraPosition().z,1) ..
       '\nPOINT_' .. teleportindex .. '_HEADING= ' .. math.floor(-ac.getCompassAngle(ac.getCameraForward()))
 
 
@@ -335,9 +332,44 @@ function script.windowMainSettings(dt)
 
   ui.text('\nweird teleports lua has access to (maybe void)')
   for i, j in pairs(ac.SpawnSet) do
-    if ui.button(i) then physics.teleportCarTo(0, j) end
-    ui.sameLine()
+    if ui.button(i) then physics.teleportCarTo(0, j) end ui.sameLine()
   end
+
+
+
+
+
+  --WEATHERFX STUFF FOR ADMINS maybe remove before update idk 
+  --i know comfy map has literally nothing to do with weather but i found myself using this a lot so i put it here
+  
+  ui.newLine(20)
+  if ac.getSim().isOnlineRace and ac.getSim().isWeatherFXActive then
+    ui.text('weather stuff & voting buttons')
+    for i= 0, 4 do
+      if ui.button('vote ' .. i) then ac.sendChatMessage('/w '..i) end
+      if i < 4 then ui.sameLine() end
+    end
+    if ui.button('check admin privileges') then ac.checkAdminPrivileges() end
+      if ac.getSim().isAdmin then --don't bother drawing if not admin
+        local weathers = {}
+        if settime==nil then settime = ac.getSim().timestamp%86400 end
+        if time_multiplier==nil then time_multiplier = ac.getSim().timeMultiplier end
+        for i, j in pairs(ac.WeatherType) do weathers[j] = i end
+        duration = ui.slider('##duration', duration, 0, 360, 'transition duration: %.0fs')
+        id, changed = ui.combo('##weathers', ac.getSim().weatherType, weathers)
+        ui.sameLine() ui.text(weathers[ac.getSim().weatherType])
+        if changed then ac.sendChatMessage('/setcspweather ' .. id .. ' ' .. duration) end
+        settime = ui.slider('##time', settime, 0, 86400, 'right click to set time: ' .. os.dateGlobal("%H:%M", settime))
+        local time = os.dateGlobal("%H:%M", settime)
+        if ui.itemClicked(1) then ac.sendChatMessage('/settime ' .. time) end
+        ui.sameLine() ui.text(os.dateGlobal("%H:%M",ac.getSim().timestamp))
+        time_multiplier = ui.slider('##time_multiplier', time_multiplier, 0, 10, 'right click to set multiplier: %.2f')
+        if ui.itemClicked(1) then ac.sendChatMessage('/set Server.TimeOfDayMultiplier ' .. math.round(time_multiplier,2)) end
+        ui.sameLine() ui.text(ac.getSim().timeMultiplier .. 'x')
+      end
+  end
+
+
 end
 
 local offsets1 = vec2()
@@ -351,14 +383,14 @@ function windowSmol(dt)
   focusedCar = ac.getCar(ac.getSim().focusedCar)
   smol_scale = smol_zoom
   size1 = image_size * smol_scale
-  offsets1:set(focusedCar.position.x, focusedCar.position.z):add(config_offset):scale(smol_scale / asd.SCALE_FACTOR):add(-
-    ui.windowSize() / 2) --autocenter
+  offsets1:set(focusedCar.position.x, focusedCar.position.z):add(config_offset):scale(smol_scale / asd.SCALE_FACTOR):add(-ui.windowSize() / 2) --autocenter
 
   rotationangle = 180 - math.deg(math.atan2(focusedCar.look.x, focusedCar.look.z))
   rotation = mat4x4.rotation(math.rad(rotationangle), vec3(0, 1, 0))
   ui.beginRotation()
   ui.drawImage(map, -offsets1, -offsets1 + size1, colors.map) --map image
   ui.endPivotRotation(rotationangle + 90, ui.windowSize() / 2)
+  
 
 
   for i = ac.getSim().carsCount - 1, 0, -1 do --draw stuff on small map
@@ -380,19 +412,20 @@ function windowSmol(dt)
         pos2 - dir2 * ts - dir2x * ts * 0.75,
         pos2 - dir2 * ts + dir2x * ts * 0.75,
         color)
-      if settings.names then
-        if not (settings.names_mouseover and not ui.windowHovered()) then
-          ui.pushFont(ui.Font.Small)
-          ui.setCursor(pos2 + namepos - ui.measureText(ac.getDriverName(i)) * 0.5)
-          ui.drawLine(pos2, pos2 + namepos , color, 2)
-          --ui.dwriteDrawText(ac.getDriverName(i),10,pos2 + namepos - ui.measureText(ac.getDriverName(i)) * 0.5,rgbm.colors.white)
-          ui.text(ac.getDriverName(i))
-          ui.popFont()
-          if ui.itemClicked() and settings.names_spectate then ac.focusCar(i) end
-        end
-      end
+      if settings.names then drawName(i,pos2, color) end
     end
   end
   ui.endOutline(rgbm(0, 0, 0, colors.map.mult), colors.map.mult^2)
 
+end
+
+function drawName(i,pos,color)
+    local name = ac.getDriverName(i)
+    ui.pushFont(ui.Font.Small)
+    ui.setCursor(pos + namepos - ui.measureText(name) * 0.5)
+    ui.drawLine(pos, pos + namepos , color, 2)
+    --ui.dwriteDrawText(name,settings.names_size,pos + namepos - ui.measureDWriteText(name,settings.names_size) * 0.5,rgbm.colors.white)
+    ui.text(name)
+    ui.popFont()
+    if ui.itemClicked() and settings.names_spectate then ac.focusCar(i) end
 end
